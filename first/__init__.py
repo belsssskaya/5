323097c1bff918e13171e603f67848e1774ba8f2
belsssskaya <kate39825@gmail.com>
2023-12-26 22:29:02 +0700

    функции

--- first/__init__.py
+++ first/__init__.py
@@ -4,19 +4,36 @@ import random
 def input_matrix(n):
     matrix = []
     print("Введите элементы матрицы:")
-    pass
+    for i in range(n):
+        row = []
+        for j in range(n):
+            try:
+                value = int(input(f"Введите значение элемента [{i}][{j}]: "))
+                row.append(value)
+            except ValueError:
+                print("Ошибка! Введите целочисленное значение!!!")
+                return input_matrix()
+        matrix.append(row)
+
     return matrix
 
 
 def generate_matrix(n):
-    pass
+    matrix = [[random.randint(1, 100) for _ in range(n)] for _ in range(n)]
+    return matrix
+
 
 
 def sum_matrices(matrix1, matrix2, n):
-    pass
+    sum_matrix = [[matrix1[i][j] + matrix2[i][j] for j in range(n)] for i in range(n)]
+    return sum_matrix
+
+
 
 def print_matrix(matrix):
-    pass
+    for row in matrix:
+        print(row)
+
 
 def main():
     while True:
