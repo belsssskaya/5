f10d087ac4ca77e6b3eacf7bb6ac9cb7965fc7e1
belsssskaya <kate39825@gmail.com>
2023-12-26 22:25:41 +0700

    представление, как будет работать программа

--- first/__init__.py
+++ first/__init__.py
@@ -4,30 +4,19 @@ import random
 def input_matrix(n):
     matrix = []
     print("Введите элементы матрицы:")
-    for i in range(n):
-        row = []
-        for j in range(n):
-            try:
-                value = int(input(f"Введите значение элемента [{i}][{j}]: "))
-                row.append(value)
-            except ValueError:
-                print("Ошибка! Введите целочисленное значение!!!")
-                return input_matrix()
-        matrix.append(row)
+    pass
     return matrix
 
 
 def generate_matrix(n):
-    matrix = [[random.randint(1, 100) for _ in range(n)] for _ in range(n)]
-    return matrix
+    pass
+
 
 def sum_matrices(matrix1, matrix2, n):
-    sum_matrix = [[matrix1[i][j] + matrix2[i][j] for j in range(n)] for i in range(n)]
-    return sum_matrix
+    pass
 
 def print_matrix(matrix):
-    for row in matrix:
-        print(row)
+    pass
 
 def main():
     while True:
@@ -38,34 +27,13 @@ def main():
         print("4. Выход")
         choice = (input("Введите номер выбранного пункта: "))
         if choice == "1":
-            n = int(input("Введите размерность матрицы: "))
-            matrix1 = input_matrix(n)
-            matrix2 = input_matrix(n)
-            print("Матрица 1:")
-            print_matrix(matrix1)
-            print("Матрица 2:")
-            print_matrix(matrix2)
+            pass
         elif choice == "2":
-            n = int(input("Введите размерность матрицы: "))
-            matrix1 = generate_matrix(n)
-            matrix2 = generate_matrix(n)
-            print("Матрица 1:")
-            print_matrix(matrix1)
-            print("Матрица 2:")
-            print_matrix(matrix2)
+            pass
         elif choice == "3":
-            try:
-                sum_matrix = sum_matrices(matrix1, matrix2, n)
-                print("Сумма матриц:")
-                print_matrix(sum_matrix)
-            except NameError:
-                print("Ошибка! Пожалуйста, сначала выберите пункт 1 или 2 для ввода или генерации матрицы.")
+            pass
         elif choice == "4":
-            print("Программа завершена.")
-            break
-        else:
-            print("Неверный выбор. Пожалуйста, выберите пункт из меню (1-4).")
-            print()
+            pass
 
 if __name__ == "__main__":
             main()
